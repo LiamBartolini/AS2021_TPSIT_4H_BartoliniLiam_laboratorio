6bb3ad4147be7753cfc692de96f23721e7babdc4
LiamBartolini <liambartolini@gmail.com>
2021-01-22 10:54:59 +0100

    v1

--- Models/Laboratorio.cs
+++ Models/Laboratorio.cs
@@ -1,18 +1,84 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.IO;
 
 namespace AS2021_TPSIT_4H_BartoliniLiam_laboratorio.Models
 {
     class Laboratorio
     {
-        List<Strumento> _strumenti = new List<Strumento>();        
-        public Laboratorio()
-        {}
+        List<Strumento> _strumenti = new List<Strumento>();
+        
+        public void AggiuntaStrumento(string descrizione, double costo, DateTime annoDiAcquisizione) => _strumenti.Add(new Strumento(descrizione, costo, annoDiAcquisizione));
 
-        public void AggiuntaStrumento(){}
-        public void EliminazioneStrumento(){}
-        public void RicercaStrumento(int numeroInventario){}
-        public void RicercaStrumento(string descrizione){}
-        public void ValoreTotaleStrumenti(DateTime inizio, DateTime fine){}
+        public void EliminazioneStrumento(int identificativo)
+        {
+            foreach (var s in _strumenti)
+                if (s.NumeroIdentificativo == identificativo)
+                    _strumenti.RemoveAt(identificativo);
+        }
+
+        public Strumento RicercaStrumento(int identificativo)
+        {
+            foreach (var s in _strumenti)
+                if (s.NumeroIdentificativo == identificativo)
+                    return s;
+            throw new Exception($"Strumento non trovato!\nIdentificativo{identificativo}");
+        }
+
+        public Strumento RicercaStrumento(string descrizione)
+        {
+            foreach (var s in _strumenti)
+                if (s.Ddescrizione == descrizione)
+                    return s;
+            throw new Exception($"Strumento non trovato!\nDescrizione{descrizione}");
+        }
+
+        public double ValoreTotaleStrumenti(DateTime inizio, DateTime fine)
+        {
+            double retVal = 0;
+            foreach (var s in _strumenti)
+                if (s.AnnoAcquisizione >= inizio && s.AnnoAcquisizione <= fine)
+                    retVal += s.Costo;
+            return retVal;
+        }
+
+        public string ElencoStrumentiInGaranzia()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var s in _strumenti)
+                if (s.AnnoScadenza <= DateTime.Today)
+                    sb.AppendLine(s.ToString());
+            return sb.ToString();
+        }
+
+        public bool SalvataggioDati()
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var s in _strumenti)
+                    sb.AppendLine(s.ToString());
+                File.AppendAllText("dati.txt", sb.ToString());
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool RipristinoDati()
+        {
+            try
+            {
+                File.WriteAllText("dati.txt", "");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
--- Models/Strumento.cs
+++ Models/Strumento.cs
@@ -10,7 +10,14 @@ namespace AS2021_TPSIT_4H_BartoliniLiam_laboratorio.Models
         double _costo;
         DateTime _annoAcquisizione;
         int _anniGaranzia;
+        DateTime _scadenzaGaranzia;
 
+        public int NumeroIdentificativo{ get => _numeroIdentificativo; }
+        public string Ddescrizione{ get => _descrizione; }
+        public DateTime AnnoAcquisizione { get => _annoAcquisizione; }
+        public Double Costo { get => _costo; }
+        public DateTime AnnoScadenza { get => _scadenzaGaranzia; }
+        
         public Strumento(string descrizione, double costo, DateTime annoDiAcquisizione)
         {
             _numeroIdentificativo = _id;
@@ -18,6 +25,7 @@ namespace AS2021_TPSIT_4H_BartoliniLiam_laboratorio.Models
             _costo = costo;
             _annoAcquisizione = annoDiAcquisizione;
             _anniGaranzia = new Random().Next(1, 5); // Genero una garazia che va da 1 a 4 anni
+            _scadenzaGaranzia = _annoAcquisizione.AddYears(_anniGaranzia);
             _id++;
         }
     }
